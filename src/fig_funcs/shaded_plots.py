28b68d4674e479a475cc476c528bb8275cb69088
ZHYMIR <dzhymir@gmail.com>
2023-12-21 17:34:12 -0500

    Added functions for shading regions.

--- src/fig_funcs/shaded_plots.py
+++ src/fig_funcs/shaded_plots.py
@@ -1,7 +1,40 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def abs_mean_shaded(data, threshold):
+def abs_mean_shaded(time, data, threshold):
     fig = plt.figure()
-    plt.plot
-    plt.fill_betweenx()
+    plt.plot(time, data)
+    max_y = np.max(data)
+    min_y = np.min(data)
+    plt.fill_between(time, min_y, max_y, where=data >= threshold, color='orange', alpha=0.2)
+    plt.fill_between(time, min_y, max_y, where=data < threshold, color='blue', alpha=0.2)
+    return fig
+
+# def abs_mean_shaded(time, data, threshold):
+#     fig = plt.figure()
+#     plt.plot(time, data)
+#     max_y = np.max(data)
+#     min_y = np.min(data)
+#     plt.fill_between(time, min_y, max_y, where=data >= threshold, color='orange', alpha=0.2)
+#     plt.fill_between(time, min_y, max_y, where=data < threshold, color='blue', alpha=0.2)
+#     return fig
+#
+# def abs_mean_shaded(time, data, threshold):
+#     fig = plt.figure()
+#     plt.plot(time, data)
+#     max_y = np.max(data)
+#     min_y = np.min(data)
+#     plt.fill_between(time, min_y, max_y, where=data >= threshold, color='orange', alpha=0.2)
+#     plt.fill_between(time, min_y, max_y, where=data < threshold, color='blue', alpha=0.2)
+#     return fig
+
+
+def frequency_center_shaded(time, data, threshold):
+    fig = plt.figure()
+    plt.plot(time, data)
+    max_y = np.max(data)
+    min_y = np.min(data)
+    plt.fill_between(time, min_y, max_y, where=data >= threshold, color='orange', alpha=0.2)
+    plt.fill_between(time, min_y, max_y, where=data < threshold, color='blue', alpha=0.2)
+    return fig
