7029697407ca1048d4a68c3083850983b16eba78
ZHYMIR <dzhymir@gmail.com>
2024-01-31 16:26:16 -0500

    Moved function to plot shock to its own module.

---
+++ src/fig_funcs/detection_plots.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+
+
+def plot_shock(time, data, shock_intervals, non_shock_intervals):
+    fig, ax = plt.subplots()
+
+    ax.plot(time, data, color='black')
+    for start, end in shock_intervals:
+        ax.axvspan(start, end, facecolor='green', alpha=0.3)
+    for start, end in non_shock_intervals:
+        ax.axvspan(start, end, facecolor='red', alpha=0.3)
+    ax.set_xlabel('Time(s)')
+    ax.set_ylabel('Acceleration(m/s/s)')
+    ax.set_title('Forced Vibration And Shock (Green=Shock, Red=Non-shock)')
+    return fig, ax
+
