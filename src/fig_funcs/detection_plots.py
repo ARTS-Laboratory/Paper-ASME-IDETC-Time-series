467213e76cdaf55199018bd585057905def9f9f1
Zhymir Thompson <dzhymir@gmail.com>
2024-05-13 13:17:25 -0400

    Added histogram plots, added plot to include mean and standard deviations to detection plot.

--- src/fig_funcs/detection_plots.py
+++ src/fig_funcs/detection_plots.py
@@ -1,24 +1,80 @@
+import itertools
 import numpy as np
 from matplotlib import pyplot as plt
+
+from utils import metrics
 
 
 def get_mean_array(time, data, shock_intervals, non_shock_intervals):
     """ """
-    mean_levels = np.zeros_like(data) # time or data, shouldn't matter
-    for (start, stop) in shock_intervals:
-        mean_levels[start:stop] = np.mean(data[start:stop])
-    for (start, stop) in non_shock_intervals:
-        mean_levels[start:stop] = np.mean(data[start:stop])
+    mean_levels = np.zeros_like(data)  # time or data, shouldn't matter
+    mean_array_helper(time, data, shock_intervals, mean_levels)
+    mean_array_helper(time, data, non_shock_intervals, mean_levels)
     return mean_levels
+
+
+def convert_intervals_to_time(time, intervals):
+    if isinstance(time, list):
+        return [(time.index(start), time.index(stop)) for start, stop in intervals]
+    elif isinstance(time, np.ndarray):
+        return [(np.where(time == start)[0][0], np.where(time == stop)[0][0]) for start, stop in intervals]
+
+
+def mean_array_helper(time, data, intervals, mean_levels):
+    """ """
+    for (start_time, stop_time) in convert_intervals_to_time(time, intervals):
+        if start_time == stop_time:
+            mean_levels[start_time] = data[start_time]
+        else:
+            mean_levels[start_time:stop_time] = np.mean(data[start_time:stop_time])
+    # if isinstance(time, list):
+    #     for (start, stop) in intervals:
+    #         start_time = time.index(start)
+    #         stop_time = time.index(stop)
+    #         if start_time == stop_time:
+    #             mean_levels[start_time] = data[start_time]
+    #         else:
+    #             mean_levels[start_time:stop_time] = np.mean(data[start_time:stop_time])
+    # elif isinstance(time, np.ndarray):
+    #     for (start, stop) in intervals:
+    #         start_time = np.where(time == start)[0][0]
+    #         stop_time = np.where(time == stop)[0][0]
+    #         if start_time == stop_time:
+    #             mean_levels[start_time] = data[start_time]
+    #         else:
+    #             mean_levels[start_time:stop_time] = np.mean(data[start_time:stop_time])
+
+
+def dev_array_helper(time, data, intervals, dev_levels):
+    """ """
+    for (start_time, stop_time) in convert_intervals_to_time(time, intervals):
+        if start_time == stop_time:
+            dev_levels[start_time] = 0.0
+        else:
+            dev_levels[start_time:stop_time] = np.std(data[start_time:stop_time])
+    # if isinstance(time, list):
+    #     for (start, stop) in intervals:
+    #         start_time = time.index(start)
+    #         stop_time = time.index(stop)
+    #         if start_time == stop_time:
+    #             dev_levels[start_time] = 0.0
+    #         else:
+    #             dev_levels[start_time:stop_time] = np.std(data[start_time:stop_time])
+    # elif isinstance(time, np.ndarray):
+    #     for (start, stop) in intervals:
+    #         start_time = np.where(time == start)[0][0]
+    #         stop_time = np.where(time == stop)[0][0]
+    #         if start_time == stop_time:
+    #             dev_levels[start_time] = 0.0
+    #         else:
+    #             dev_levels[start_time:stop_time] = np.std(data[start_time:stop_time])
 
 
 def get_deviation_array(time, data, shock_intervals, non_shock_intervals):
     """ """
     dev_levels = np.zeros_like(data) # time or data, shouldn't matter
-    for (start, stop) in shock_intervals:
-        dev_levels[start:stop] = np.std(data[start:stop])
-    for (start, stop) in non_shock_intervals:
-        dev_levels[start:stop] = np.std(data[start:stop])
+    dev_array_helper(time, data, shock_intervals, dev_levels)
+    dev_array_helper(time, data, non_shock_intervals, dev_levels)
     return dev_levels
 
 
@@ -29,33 +85,80 @@ def get_std_ratio(time, data, shock_intervals, non_shock_intervals):
     return devs / base_val
 
 
-def plot_shock(time, data, shock_intervals, non_shock_intervals):
-    fig, ax = plt.subplots()
+def interval_histogram(time, data, shock_intervals, non_shock_intervals, title=True):
+    """ """
+    shock_times = convert_intervals_to_time(time, shock_intervals)
+    non_shock_times = convert_intervals_to_time(time, non_shock_intervals)
+    fig, ax = plt.subplots(ncols=2)
+    ax[0].hist([metrics.abs_mean(data[start:stop]) for start, stop in shock_times])
+    ax[1].hist([metrics.abs_mean(data[start:stop]) for start, stop in non_shock_times])
+    return fig
 
-    ax.plot(time, data, color='black')
-    for start, end in shock_intervals:
-        ax.axvspan(start, end, facecolor='green', alpha=0.3)
-    for start, end in non_shock_intervals:
-        ax.axvspan(start, end, facecolor='red', alpha=0.3)
-    ax.set_xlabel('Time(s)')
-    ax.set_ylabel('Acceleration(m/s\u00b2)')
-    ax.set_title('Forced Vibration And Shock (Green=Shock, Red=Non-shock)')
+
+def raw_histogram(time, data, shock_intervals, non_shock_intervals, title=True):
+    """ """
+    n_bins = 50
+    shock_times = convert_intervals_to_time(time, shock_intervals)
+    non_shock_times = convert_intervals_to_time(time, non_shock_intervals)
+    # These flatten the list of lists into one big list
+    # shock_vals = [point for start, stop in shock_times for point in data[start:stop]]
+    # non_shock_vals = [point for start, stop in non_shock_times for point in data[start:stop]]
+    fig, ax = plt.subplots(ncols=2, sharey=True)
+    ax[0].hist(np.array(list(itertools.chain([data[start:stop] for start, stop in shock_times]))), bins=n_bins)
+    ax[1].hist(np.array(list(itertools.chain([data[start:stop] for start, stop in non_shock_times]))), bins=n_bins)
+    return fig
+
+
+def plot_shock_helper(interval, color, alpha, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    [ax.axvspan(start, end, facecolor=color, alpha=alpha) for start, end in interval]
+    # for start, end in interval:
+    #     ax.axvspan(start, end, facecolor=color, alpha=alpha)
+
+
+def plot_shock(time, data, shock_intervals, non_shock_intervals, title=True):
+    fig = plt.figure(figsize=(6.5, 2), layout='compressed')
+    # Plot acceleration
+    plt.plot(time, data, color='black')
+    # Shade regions
+    plot_shock_helper(shock_intervals, 'red', 0.3)
+    plot_shock_helper(non_shock_intervals, 'blue', 0.3)
+    # for start, end in shock_intervals:
+    #     plt.axvspan(start, end, facecolor='red', alpha=0.3)
+    # for start, end in non_shock_intervals:
+    #     plt.axvspan(start, end, facecolor='blue', alpha=0.3)
+    # Setting plot details
+    plt.xlim((time[0], time[-1]))
+    plt.xlabel('time (s)')
+    plt.ylabel('acceleration (m/s\u00b2)')
+    if title:
+        plt.title('Forced Vibration And Shock (Blue=Non-shock, Red=Shock)')
+    # plt.tight_layout()
+    return fig
+
+
+def plot_shock_w_mean_std(time, data, shock_intervals, non_shock_intervals):
+    means = get_mean_array(time, data, shock_intervals, non_shock_intervals)
+    devs = get_deviation_array(time, data, shock_intervals, non_shock_intervals)
+    fig = plt.figure()
+    # Plot acceleration
+    plt.plot(time, data, color='black')
+    plt.plot(time, means, '--', color='black')
+    plt.plot(time, means + devs, ':', color='black')
+    plt.plot(time, means - devs, ':', color='black')
+    # Shade regions
+    plot_shock_helper(shock_intervals, 'red', 0.3)
+    plot_shock_helper(non_shock_intervals, 'blue', 0.3)
+    # for start, end in shock_intervals:
+    #     plt.axvspan(start, end, facecolor='red', alpha=0.3)
+    # for start, end in non_shock_intervals:
+    #     plt.axvspan(start, end, facecolor='blue', alpha=0.3)
+    # Setting plot details
+    plt.xlim((time[0], time[-1]))
+    plt.xlabel('time (s)')
+    plt.ylabel('acceleration (m/s\u00b2)')
+    plt.title('Forced Vibration And Shock (Blue=Shock, Red=Non-shock)')
     plt.tight_layout()
-    return fig, ax
-
-
-def plot_shock_v2(time, data, shock_intervals, non_shock_intervals):
-    fig, ax = plt.subplots()
-
-    ax.plot(time, data, color='black')
-    for start, end in shock_intervals:
-        ax.axvspan(start, end, facecolor='blue', alpha=0.3)
-    for start, end in non_shock_intervals:
-        ax.axvspan(start, end, facecolor='orange', alpha=0.3)
-    ax.set_xlabel('time (s)')
-    ax.set_ylabel('acceleration (m/s\u00b2)')
-    ax.set_title('Forced Vibration And Shock')
-    plt.legend()
-    plt.tight_layout()
-    return fig, ax
+    return fig
 
