ca7836f777e9a9bd1317e82ece07b714ef760694
Zhymir Thompson <dzhymir@gmail.com>
2024-05-13 13:22:28 -0400

    Added time unit option (sec vs msec) and changed layout to constrained.

--- src/fig_funcs/signal_plots.py
+++ src/fig_funcs/signal_plots.py
@@ -5,18 +5,20 @@ import matplotlib.pyplot as plt
 from utils import metrics
 
 
-def plot_signal(time, data, ms=False):
-    fig = plt.figure()
+def plot_signal(time, data, ms=False, fig_size=None):
+    fig = plt.figure(layout='constrained') if fig_size is None else plt.figure(figsize=fig_size, layout='constrained')
     ylabel = 'acceleration (m/s\u00b2)'
     if ms:
         xlabel = 'time (\u03bcs)'
         plt.plot(time * 100, data)
+        plt.xlim([100 * time[0], 100 * time[-1]])
     else:
         xlabel = 'time (s)'
         plt.plot(time, data)
+        plt.xlim([time[0], time[-1]])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.tight_layout()
+    # plt.tight_layout()
     return fig
 
 
