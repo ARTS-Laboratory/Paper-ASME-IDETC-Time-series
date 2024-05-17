0bc0be5dc200e47482024c99477093e49a9aeea4
Zhymir Thompson <dzhymir@gmail.com>
2024-05-17 12:36:39 -0400

    added model type parameter to functions.

--- src/offline_detection/binary_segmentation.py
+++ src/offline_detection/binary_segmentation.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def guess_breaks(signal):
-    model = "l2"
+def guess_breaks(signal, model_type='l2'):
+    model = model_type  # "l2"
     noise = 10
     alg = rpt.Binseg(model=model).fit(signal)
     my_bkps_pen = alg.predict(epsilon=3.5 * len(signal) * noise ** 2)
     return my_bkps_pen
 
 
-def get_breaks(signal, num_bkps):
-    model = "l2"
+def get_breaks(signal, num_bkps, model_type='l2'):
+    model = model_type  # "l2"
     alg = rpt.Binseg(model=model).fit(signal)
     my_bkps = alg.predict(n_bkps=num_bkps)
     return my_bkps
--- src/offline_detection/bottom_up.py
+++ src/offline_detection/bottom_up.py
@@ -2,13 +2,13 @@ import ruptures as rpt
 import matplotlib.pyplot as plt
 
 
-def get_breaks(signal, num_bkps):
-    model = "l2"
+def get_breaks(signal, num_bkps, model_type='l2'):
+    model = model_type  # "l2"
     alg = rpt.BottomUp(model=model).fit(signal)
     my_bkps = alg.predict(num_bkps)
     return my_bkps
 
 
-def plot_breaks(signal, bkps, my_bkps):
+def plot_breaks(signal, my_bkps):
     rpt.show.display(signal, my_bkps)
     plt.show()
