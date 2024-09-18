39d97ee13a5ee327ea3d075a44c49833bd536350
Zhymir Thompson <dzhymir@gmail.com>
2024-09-18 12:02:26 -0400

    Removed redundant functions.

--- src/fig_funcs/shaded_plots.py
+++ src/fig_funcs/shaded_plots.py
@@ -11,24 +11,6 @@ def abs_mean_shaded(time, data, threshold):
     plt.fill_between(time, min_y, max_y, where=data < threshold, color='blue', alpha=0.2)
     return fig
 
-# def abs_mean_shaded(time, data, threshold):
-#     fig = plt.figure()
-#     plt.plot(time, data)
-#     max_y = np.max(data)
-#     min_y = np.min(data)
-#     plt.fill_between(time, min_y, max_y, where=data >= threshold, color='orange', alpha=0.2)
-#     plt.fill_between(time, min_y, max_y, where=data < threshold, color='blue', alpha=0.2)
-#     return fig
-#
-# def abs_mean_shaded(time, data, threshold):
-#     fig = plt.figure()
-#     plt.plot(time, data)
-#     max_y = np.max(data)
-#     min_y = np.min(data)
-#     plt.fill_between(time, min_y, max_y, where=data >= threshold, color='orange', alpha=0.2)
-#     plt.fill_between(time, min_y, max_y, where=data < threshold, color='blue', alpha=0.2)
-#     return fig
-
 
 def frequency_center_shaded(time, data, threshold):
     fig = plt.figure()
