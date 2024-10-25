b639fe4e2c1016fc76f9e67279d0ef30cafa0712
Zhymir Thompson <dzhymir@gmail.com>
2024-10-25 02:19:40 -0400

    added docstring and improved error statement.

--- src/utils/write_data.py
+++ src/utils/write_data.py
@@ -3,10 +3,16 @@ from pathlib import Path
 
 
 def save_path(path_name):
+    """ Convert path_name to Path object.
+
+        :param path_name: Path to convert.
+        :type path_name: str | Path | None
+        :return: Converted path.
+    """
     if isinstance(path_name, Path):
         return path_name
     elif path_name is None or path_name == '':
         return Path(os.curdir)
     elif isinstance(path_name, str) and os.path.exists(path_name):
         return Path(path_name)
-    raise ValueError
+    raise ValueError(f'{path_name} is not a valid or existing path.')
