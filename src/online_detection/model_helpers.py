0b8e6b289c5c973e5243b1ae1557a7a327e223bf
Zhymir Thompson <dzhymir@gmail.com>
2024-12-30 17:55:14 -0500

    added function that does same thing as v1.

--- src/online_detection/model_helpers.py
+++ src/online_detection/model_helpers.py
@@ -46,3 +46,28 @@ def detection_to_intervals_for_generator_v2(time_vec, begin, model_generator):
     else:
         nonshocks.append((time_vec[begin], time_vec[-1]))
     return shocks, nonshocks
+
+
+
+def detection_to_intervals_for_generator_v3(
+        time_vec, begin, model_generator, start_offset=0):
+    """ Convert detections from generator to time intervals
+
+        This version is for detecting when a measure deviates from expected.
+    """
+    shock = False
+    shocks = list()
+    nonshocks = list()
+    for idx, is_change in enumerate(model_generator, start=start_offset):
+        if is_change ^ shock:
+            region = (time_vec[begin], time_vec[idx])
+            shock = not shock
+            begin = idx
+            if is_change:  # change point detected, and not in shock
+                nonshocks.append(region)
+            else:  # no change point detected, and in shock
+                shocks.append(region)
+    if shock:
+        shocks.append((time_vec[begin], time_vec[-1]))
+    else:
+        nonshocks.append((time_vec[begin], time_vec[-1]))
