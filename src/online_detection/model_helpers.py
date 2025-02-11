09ea05b2a274749ae5dab02d350ef89a9bf104e2
Zhymir Thompson <dzhymir@gmail.com>
2025-02-11 15:31:58 -0500

    added function to run detection algorithm with progress bar.

--- src/online_detection/model_helpers.py
+++ src/online_detection/model_helpers.py
@@ -1,3 +1,11 @@
+import warnings
+
+try:
+    from tqdm import tqdm
+except ModuleNotFoundError:
+    warnings.warn('tqdm module not included in environment.')
+except ImportError:
+    warnings.warn('Expected Cython imports to be available.')
 
 
 def detection_to_intervals_for_generator_v1(
@@ -23,6 +31,17 @@ def detection_to_intervals_for_generator_v1(
     else:
         nonshocks.append((time_vec[begin], time_vec[-1]))
     return shocks, nonshocks
+
+
+def detection_to_intervals_for_generator_v1_with_progress(
+        time_vec, begin, model_generator, iter_len, start_offset=0):
+    """ Convert detections from generator to time intervals."""
+    try:
+        my_generator = tqdm(model_generator, total=iter_len)
+        return detection_to_intervals_for_generator_v1(time_vec, begin, my_generator, start_offset)
+    except NameError:
+        warnings.warn('tqdm module not included in environment.\ndefaulting to run without progress.')
+        return detection_to_intervals_for_generator_v1(time_vec, begin, model_generator, start_offset)
 
 
 def detection_to_intervals_for_generator_v2(time_vec, begin, model_generator):
