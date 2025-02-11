01aaa7afae0efff380b9b584b8b43391f5a50945
Zhymir Thompson <dzhymir@gmail.com>
2025-02-11 15:37:16 -0500

    changed plot_shock figure to new format. includes legend and removes title.

--- src/fig_funcs/detection_plots.py
+++ src/fig_funcs/detection_plots.py
@@ -47,20 +47,41 @@ def plot_shock_helper(ax, interval, color, alpha):
         alpha=alpha, facecolor=color, transform=ax.get_xaxis_transform()))
 
 
-def plot_shock(time, data, shock_intervals, non_shock_intervals, title=True):
-    fig = plt.figure(figsize=(6.5, 2), layout='compressed')
-    # Plot acceleration
-    plt.plot(time, data, color='black')
-    plt.xlim((time[0], time[-1]))
-    ax = plt.gca()
-    # Shade regions
-    plot_shock_helper(ax=ax, interval=shock_intervals, color='red', alpha=0.3)
-    plot_shock_helper(ax=ax, interval=non_shock_intervals, color='blue', alpha=0.3)
-    # Setting plot details
-    plt.xlabel('time (s)')
-    plt.ylabel('acceleration (m/s\u00b2)')
-    if title:
-        plt.title('Forced Vibration And Shock (Blue=Non-shock, Red=Shock)')
+def plot_shock(time, data, shock_intervals, non_shock_intervals, to_ms=False):
+    # fig = plt.figure(figsize=(6.5, 2), layout='compressed')
+    # ax = plt.gca()
+    fig, ax = plt.subplots(figsize=(6.5, 2), layout='compressed')
+    safe_color = 'blue'
+    unsafe_color = 'red'
+    shade_alpha = 0.3
+    y_min = -200
+    y_max = 200
+    if to_ms:
+        scalar = 1_000
+        ms_time = time * scalar
+        # Plot acceleration
+        ax.plot(ms_time, data, color='black')
+        ax.set_xlim((ms_time[0], ms_time[-1]))
+        new_shocks = ((start * scalar, stop * scalar) for start, stop in shock_intervals)
+        new_non_shocks = ((start * scalar, stop * scalar) for start, stop in non_shock_intervals)
+        plot_shock_helper(ax=ax, interval=new_shocks, color=unsafe_color, alpha=shade_alpha)
+        plot_shock_helper(ax=ax, interval=new_non_shocks, color=safe_color, alpha=shade_alpha)
+        ax.set_xlabel('time (ms)')
+        ax.set_ylabel('acceleration (m/s\u00b2)')
+    else:
+        # Plot acceleration
+        ax.plot(time, data, color='black')
+        ax.set_xlim((time[0], time[-1]))
+        # Shade regions
+        plot_shock_helper(ax=ax, interval=shock_intervals, color=unsafe_color, alpha=shade_alpha)
+        plot_shock_helper(ax=ax, interval=non_shock_intervals, color=safe_color, alpha=shade_alpha)
+        # Setting plot details
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('acceleration (m/s\u00b2)')
+    ax.set_ylim((y_min, y_max))
+    safe_rect = Rectangle((0, 0), 1, 1, facecolor=safe_color, alpha=shade_alpha)
+    unsafe_rect = Rectangle((0, 0), 1, 1, facecolor=unsafe_color, alpha=shade_alpha)
+    ax.legend((safe_rect, unsafe_rect), ('normal region', 'shock region'), bbox_to_anchor=(1, 1), loc='lower right', ncol=2)
     # plt.tight_layout()
     return fig
 
